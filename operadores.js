8324f19376b250ef19496af752b6ad0f5a99b18a
CarlosHeraclito <ch.vasconcelos.paiva@gmail.com>
2025-01-13 22:02:17 -0300

    organização rquivos primeiraaula js

---
+++ operadores.js
@@ -0,0 +1,4 @@
+let salariodoUsuario = prompt('Qual o seu lsalario')
+salarioUsuario = Number(salarioUsuario)
+salarioUsuaro = salarioUsuario + 1000
+console.log(salarioUsuario)
---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('Olá Mundo')
+console.log("Olá Mundo")
+
+alert('Olá Mundo')
+
+let nome = 'carlos'
+let idade = 39
+let = email = 'ch.vasconcelos.paiva@gmail.com'
+const datadenascimento = '16/16/1900'
+
+console.log(nome)
+console.log(idade)
+console.log(email)
+console.log(datadenascimento)
+
+nome = prompt('qual o seu nome?')
+idade = prompt('Qual sua idade?')
+email = prompt('qual seu email?')
+
+console.log(nome, idade, email)
